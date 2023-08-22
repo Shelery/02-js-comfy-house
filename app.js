5258f0c92854e374d6e92d0ff5a446bafb0f5a37
Shelery <92330177+Shelery@users.noreply.github.com>
2023-08-22 15:17:34 +0200

    Match JS variable with DOM elements, set up Classes

---
+++ app.js
@@ -0,0 +1,31 @@
+// Variable
+// Find all the call-to-action buttons
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCart = document.querySelector(".clear-cart");
+// Find the "platforms" you will want to modify
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+// Main CART
+let cart = [];
+
+// Create Classes (objects)
+// Getting the products
+class Products{
+
+}
+// UI classes
+// displaying products
+class UI {
+
+}
+// Local Storage class
+class Storage{
+
+}
+
+// Event Listeners
